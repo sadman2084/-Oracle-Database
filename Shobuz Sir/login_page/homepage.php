808462bf2ab04ac3d6c7ffee93e28d3392b92266
Sadmankabir2084 <123572457+sadman2084@users.noreply.github.com>
2025-02-17 01:23:04 +0600

    new php project

---
+++ Shobuz Sir/login_page/homepage.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    
+
+<h1>Welcome to Your Profile</h1>
+    <p>This is your profile page.</p>
+    <a href="index.php">Go Back to Home</a><br>
+    <a href="q1.html">query 1</a><br>
+    <a href="q2.html">query 2</a><br>
+    <a href="q3.html">query 3</a><br>
+
+
+</body>
+</html>
